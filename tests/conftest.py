3acb9ae9dab23c0f2f38b1e7ea5b7179992f562d
Olga B <o.lgaromanovna@yandex.ru>
2023-12-15 18:25:01 +0300

    fixed files

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,34 @@
 import pytest
-from selene.support import shared as _shared
+from allure_commons._allure import attach
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selene import Browser, Config
 
 
-@pytest.fixture(scope="function", autouse=True)
-def open_browser():
-    browser = _shared.browser
-    browser.config.base_url = 'https://demoqa.com'
-    browser.config.timeout = 10
-    browser.config.window_width = 1200
-    browser.config.window_height = 1000
+@pytest.fixture(scope='function')
+def open_browser(request):
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
 
-    yield
+    browser = Browser(Config(driver))
+    yield browser
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
 
     browser.quit()
