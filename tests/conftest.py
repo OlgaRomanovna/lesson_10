ff2a6e3d0feacdea23e7d431fde83be52c1e9f0b
Olga B <o.lgaromanovna@yandex.ru>
2023-12-21 20:57:14 +0300

    changed file

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,34 +1,58 @@
+import os
+
 import pytest
 
-from selene import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selene import browser
+import attach
+from dotenv import load_dotenv
 
-from tests import attach
+
+DEFAULT_BROWSER_VERSION = '100.0'
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser-version',
+        help='Версия браузера в которой будут запущены тесты',
+        default='100.0'
+    )
+
+
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
 
 
 @pytest.fixture(scope='function', autouse=True)
-def open():
+def setup_browser(request):
+    browser_version = request.config.getoption('--browser-version')
+    browser_version = browser_version if browser_version != '' else DEFAULT_BROWSER_VERSION
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": "100.0",
+        "browserVersion": browser_version,
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
         }
     }
     options.capabilities.update(selenoid_capabilities)
+
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+    remote_browser_url = os.getenv('REMOTE_BROWSER_URL')
+
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{login}:{password}@{remote_browser_url}",
         options=options
     )
 
     browser.config.driver = driver
-
-    browser.config.base_url = 'https://demoqa.com'
-    browser.config.window_width = 1280
+    browser.config.base_url = "https://demoqa.com"
     browser.config.window_height = 1200
+    browser.config.window_width = 1600
 
     yield browser
 
